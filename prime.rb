9920211f14c6aa706bd241388013462331835e8a
Madeline Stark <madeline.stark1@gmail.com>
2018-03-26 18:33:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,10 @@
 def prime?(integer)
-  array = []
-  array << integer % (2..2000).to_a
     if
       integer < 2
       false
     elsif integer == 2
-      true 
-    elsif
-      array.include?(0)
-      false
-    else
+      true
+    elsif integer % (2..integer).to_a !== 0
       true
     end
 end
-
