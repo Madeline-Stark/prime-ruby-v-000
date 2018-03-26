5f2130991e5f7dae3b0585f977b76abd3173b98b
Madeline Stark <madeline.stark1@gmail.com>
2018-03-26 18:05:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,27 +1,13 @@
 def prime?(integer)
-  #numbers = (2..100).to_a
- #prime =
-  #numbers.any? do |number|
     if
       integer < 2
       false
     elsif
-      integer % 4 == 0
-      false
-    elsif
-      integer % 6 == 0
-      false
-    elsif
-      integer % 5 == 0
-      false
-    elsif
-      integer % 7 == 0
+      integer % (5..2000).to_a == 0
       false
     else
       true
     end
-  #end
-  #prime
 end
 #what conditions can we just write off right a way
 #just need to see remainder from some main numbers
