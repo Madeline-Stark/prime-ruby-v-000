028d6152aac346816ee028f3b286e64f21dbd0f6
Madeline Stark <madeline.stark1@gmail.com>
2018-03-26 17:55:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+def prime?(integer)
+  #numbers = (2..100).to_a
+ #prime =
+  numbers.any? do |number|
+    if
+      integer < 1
+      false
+    elsif
+      integer % 2 == 0
+      false
+    elsif
+      integer % 3 == 0
+      false
+    elsif
+      integer % 5 == 0
+      false
+    elsif
+      integer % 7 == 0
+      false
+    else
+      true
+    end
+  end
+  #prime
+end
+#what conditions can we just write off right a way
+#just need to see remainder from some main numbers
+#look up how to deal with a range in ruby
